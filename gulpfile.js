0935f7fafbe9ea4938e1d23f638bdf44d92eb9b6
userlidong <1491326913@qq.com>
2019-10-12 11:15:12 +0800

    sss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,77 @@
+
 const gulp = require("gulp");
-const sass = require("gulp-sass");
 
-// 监听任务
-gulp.task("watchall",async ()=>{
-    //定义一个监听器，监听文件是否有改变，如果有改变，则自动拷贝。
-    gulp.watch("*.html",async ()=>{
-        //把当前目录下的所有html文件拷贝至目录：dist
+// const uglify = require("gulp-uglify")
+
+//引入合并js文件插件
+// const concat = require("gulp-concat")
+
+//引入合并/压缩/崇明js文件
+// const rename = require("gulp-rename")
+
+//引入css压缩插件
+const sass = require("gulp-sass")
+
+//引入sass插件
+// const sass = require("gulp-connect");
+
+//引入简易服务器插件
+// const connect = require("gulp-connect");
+
+
+
+
+//这是gulp要完成的任务
+//定义一个复制任务
+// gulp.task('copyallfile', async ()=>{
+//     //把当前目录下的所有文件拷贝至目录：D:\\phpStudy\\WWW\\mycasio下；
+//     gulp.src('*/**/*')
+//     .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio"));
+// })
+
+//定义一个监听器，监听文件是否有改变，如果有改变就自动改变
+gulp.task("watchall", async ()=>{
+    gulp.watch("*.html", async ()=>{
         gulp.src("*.html")
-        .pipe(gulp.dest("d:\\phpstudy\\www\\mycasio1"));
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio"));
+    }); 
+
+    gulp.watch("php/*.php", async ()=>{
+        gulp.src("php/*.php")
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\php"));
+    }); 
+
+    gulp.watch("js/*.js",async ()=>{
+        gulp.src("js/*.js")
+        // .pipe(uglify())
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\js"));
     });
 
-    // 监听sass文件
-    gulp.watch("sass/**/*",async ()=>{
-        //把当前目录下的所有html文件拷贝至目录
-        gulp.src("sass/**/*")
-        .pipe(sass())
-        .pipe(gulp.dest("d:\\phpstudy\\www\\mycasio1\\css"));
-    });
+    // gulp.watch(["js/index.js","js/goods.js"],async ()=>{
+    //     gulp.src(["js/index.js","js/goods.js"])
+    //     .pipe(concat("commons.js"))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\js"));
+    // })
+
+    // gulp.watch(["js/index.js","js/goods.js"],async ()=>{
+    //     gulp.src(["js/index.js","js/goods.js"])
+    //     .pipe(concat("commons.js"))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\js"))
+    //     .pipe(rename("commons.min.js"))
+    //     .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\js"));
+    // })
+
+    gulp.watch("css/*.css", async ()=>{
+        gulp.src("css/*.css")
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\css"));
+    })
+
+    // gulp.task("sass", async ()=>{
+    //     gulp.src("sass/**/*.scss")
+    //     .pipe(sass())
+    //     .pipe(gulp.dest("D:\\phpStudy\\WWW\\mycasio\\css"));
+    // })
+
 });
